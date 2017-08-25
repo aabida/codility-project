a246a60babd2ba4ce59c584ab6b82993a04d7c40
aabida <aabida@users.noreply.github.com>
2017-08-25 17:53:33 +0200

    Remove unnecessary variables

--- test/lesson-4-frog-river-one.specs.js
+++ test/lesson-4-frog-river-one.specs.js
@@ -8,9 +8,6 @@ var sut = require('../src/lesson-4-frog-river-one.js');
 
 describe('Frog river one', function() {
 		it('should return the expected result by the official site for the very first case', function() {
-			var operationsArray = [3, 4, 4, 6, 1, 4, 4];
-			var counterSize = 5;
-
 			var actual = sut.solution(5, [1,3,1,4,2,3,5,4]);
 			assert.equal(6, actual);
 		});
